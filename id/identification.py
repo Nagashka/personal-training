a068cfe6857816ec49426470fbedd70f03b8a2f3
Nagashka <edr.potdevin@gmail.com>
2025-12-10 18:11:43 +0000

    identification update

--- id/identification.py
+++ id/identification.py
@@ -9,14 +9,19 @@ client_secret = os.getenv('client_secret')
 client_id = os.getenv('client_id')
 grant_type = os.getenv('grant_type')
 
-token = None
+class _id():
+    def __init__(self, url):
+        self.url = url
+        self.client_secret = client_secret
+        self.client_id = client_id
+        self.data = {"grant_type": grant_type, "client_id": client_id, "client_secret": client_secret}
+        self.token = self.get_token()
 
-def get_token(url):
-        data = {"grant_type": grant_type, "client_id": client_id, "client_secret": client_secret}
-        token_response = requests.post(url + 'oauth/token', data=data) #Get token
-        if token_response.status_code == 200: #Token validated
-            token = token_response.json().get('access_token')
-        else: #Token Echec
-            token = None
-            print("Token error:", token_response.text)
-        return token
+    def get_token(self):
+            self.token_response = requests.post(url + 'oauth/token', data=self.data) #Get token
+            if self.token_response.status_code == 200: #Token validated
+                token = token_response.json().get('access_token')
+            else: #Token Echec
+                token = None
+                print("Token error:", token_response.text)
+            return token
